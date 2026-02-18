3de9e610a8443dcd3749a8abc970e3837131cff0
vanshsharma-tech <vanshsharma9431@gmail.com>
2026-02-18 12:05:38 +0530

    first commit

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
 number_of_students = 125
 student = 30
 remain_student = number_of_students % 30
-student_in_class = number_of_students - remain_student
-print("The students which are in the class is ",student_in_class)
+total_class = 125 // 30
+print("The Total class formed is ",total_class)
 print("The remain students is ",remain_student)
