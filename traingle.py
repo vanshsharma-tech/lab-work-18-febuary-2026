d9fd2b89e239caf17c6ce03bfc641b0d477db8fc
vanshsharma-tech <vanshsharma9431@gmail.com>
2026-02-18 15:52:09 +0530

    traingle

--- traingle.py
+++ traingle.py
@@ -1,9 +1,21 @@
 first_angle = int(input("Enter the first angle:"))
-second_angle = int(input("Enter the second angle:"))
-third_angle = int(input("Enter the third angle:"))
-
-traingle = first_angle + second_angle + third_angle
-if (first_angle>0 and second_angle>0 and third_angle>0) and (traingle == 180):
-    print("It is in traingle form")
+if first_angle<=0:
+    print("Angle can't be a negative or zero value")
+    exit(0)
 else:
-    print("It is not in traingle form")
+    second_angle = int(input("Enter the second angle:"))
+    if second_angle<=0:
+        print("Angle can't be a negative or zero value")
+        exit(0)
+    else:
+        third_angle = int(input("Enter the third angle:"))
+        if third_angle<=0:
+            print("Angle can't be a negative or zero value")
+            exit(0)
+        else:
+            traingle = first_angle + second_angle + third_angle
+            if (first_angle>0 and second_angle>0 and third_angle>0) and (traingle == 180):
+                print("It is in traingle form")
+            else:
+                print("It is not in traingle form")
+
