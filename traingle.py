a92d1f7fc772d3093bf952cef6f7a7d374576825
vanshsharma-tech <vanshsharma9431@gmail.com>
2026-02-18 14:56:15 +0530

    traingle

---
+++ traingle.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first angle:"))
+b = int(input("Enter the second angle:"))
+c = int(input("Enter the third angle:"))
+
+angle = a + b + c
+
+if angle == 180:
+    print("It is in traingle form")
+else:
+    print("It is not in traingle")
