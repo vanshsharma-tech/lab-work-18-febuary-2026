050efdd276be2aa8d6182457d015d8b0b1c364de
vanshsharma-tech <vanshsharma9431@gmail.com>
2026-02-24 12:15:02 +0530

    first commit

---
+++ Agenda/31_print_prime_num.py
@@ -0,0 +1,23 @@
+# Take the input of range from user
+number = int(input("Enter the range: "))
+# Validate the range is not a negative value
+if number < 0:
+    print("Prime number is not defined for negative range")
+    exit()
+
+
+# Create a is_prime_num to check the number is prime or not
+def is_prime_num(n):
+    count = 0
+    for i in range(2, n):
+        # Checks if the number is divided by any value between the 2 and number then returns the false
+        if n % i == 0:
+            return False
+
+    return True
+
+
+# Print result
+for i in range(1, number + 1):
+    if is_prime_num(i):
+        print(i, end=" ")
